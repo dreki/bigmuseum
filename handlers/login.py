6a1fb1fc48a2f58ab5db609c417286d792538433
Sean Gilbertson <sean.gilbertson@gmail.com>
2021-12-01 00:52:12 -0600

    Create new User record

--- handlers/login.py
+++ handlers/login.py
@@ -1,6 +1,13 @@
+from typing import Optional
 import httpx
+# from asyncpraw.models import User
+from asyncpraw.models import Redditor
+from asyncpraw.reddit import Reddit
 from db import AIOEngine, get_engine
 from models.session import RedditCredentials, Session
+from models.user import User
+from tornado.web import HTTPError
+from utils.log import logger
 
 from handlers.base import BaseHandler
 
@@ -37,6 +44,29 @@ class FinishLoginHandler(BaseHandler):
             credentials: RedditCredentials = RedditCredentials.parse_obj(response.json())
             session.reddit_credentials = credentials
             await engine.save(session)
+
+            # Get Reddit user's ID
+            reddit: Reddit = await self.make_reddit_client()
+
+            # reddit_user: User = reddit.user
+            # # user: str = await reddit.get_me()
+            # logger.debug(f'> user id {reddit_user}')
+            # # logger.debug(dir(reddit_user))
+            # logger.debug(vars(reddit_user))
+            logger.debug(await reddit.user.me())
+            current_redditor: Optional[Redditor] = await reddit.user.me()
+            if not current_redditor:
+                raise HTTPError(status_code=500, reason='Failed to get Redditor information.')
+            reddit_username: str = current_redditor.name
+            # Find a User if it exists.
+            user: Optional[User] = await engine.find_one(User, User.username == reddit_username)
+            if not user:
+                user = User(username=reddit_username)
+            await engine.save(user)
+            # if user:
+            #     user.reddit_credentials = credentials
+            # else:
+
             self._reddit_credentials = credentials
         self.redirect(url=self.reverse_url('app', ''))
 
---
+++ models/user.py
@@ -0,0 +1,8 @@
+"""Holds the User model."""
+from models.model import Model
+
+
+class User(Model):
+    """User model."""
+
+    username: str
